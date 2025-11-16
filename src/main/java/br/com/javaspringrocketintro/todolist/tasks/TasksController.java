1d226a1a73a5803ce768193cf847e08b3e8c487f
Dev-LDRC <lucasrabelocruz@gmail.com>
2025-11-15 22:30:32 -0300

    fix: fixing bug of method PUT not work as expected

--- src/main/java/br/com/javaspringrocketintro/todolist/tasks/TasksController.java
+++ src/main/java/br/com/javaspringrocketintro/todolist/tasks/TasksController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.javaspringrocketintro.todolist.utils.Utils;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -56,34 +56,51 @@ public class TasksController {
    @PutMapping("/{taskId}")
    public ResponseEntity<Object> update(@RequestBody TasksModel tasksModel, HttpServletRequest request, @PathVariable String taskId) {
 
-      var taskToUpdate = this.tasksRepository.findById(taskId).orElse(null);
-      
-      if (taskToUpdate == null) {
+      var task = this.tasksRepository.findById(taskId).orElse(null);
+
+      if (task == null) {
          return ResponseEntity.status(400).body("Esta tarefa não existe.");
       }
 
       var userId = request.getAttribute("userId");
 
-      if (!taskToUpdate.getUserId().equals(userId.toString())) {
+      if (!task.getUserId().equals(userId.toString())) {
          return ResponseEntity.status(400).body("Não autorizado para alterar esta tarefa.");
       }
 
+      if (tasksModel.getTitle() != null) {
+         try {
+            task.setTitle(tasksModel.getTitle());
+         } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+         }
+      }
+      if (tasksModel.getDescription() != null) {
+         task.setDescription(tasksModel.getDescription());
+      }
+      if (tasksModel.getStartAt() != null) {
+         task.setStartAt(tasksModel.getStartAt());
+      }
+      if (tasksModel.getEndAt() != null) {
+         task.setEndAt(tasksModel.getEndAt());
+      }
+      if (tasksModel.getPriority() != null) {
+         task.setPriority(tasksModel.getPriority());
+      }
+
       var currentDate = LocalDateTime.now();
-      if (currentDate.isAfter(tasksModel.getStartAt()) || currentDate.isAfter(tasksModel.getEndAt())) {
+      if (currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getEndAt())) {
          return ResponseEntity.status(400).body("A data e hora de inicio deve ser presente ou futura.");
       }
-      if (tasksModel.getStartAt().isAfter(tasksModel.getEndAt())) {
+      if (task.getStartAt().isAfter(task.getEndAt())) {
          return ResponseEntity.status(400).body("A data e hora de inicio deve vir antes da data e hora de fim.");
       }
-
-      Utils.copyNonNullProperties(tasksModel, taskToUpdate);
       
-      taskToUpdate.setUpdatedAt(LocalDateTime.now());
+      task.setUpdatedAt(LocalDateTime.now());
 
-      var taskUpdated = tasksRepository.save(taskToUpdate);
+      var taskUpdated = this.tasksRepository.save(task);
 
       return ResponseEntity.status(200).body(taskUpdated);
-
    }
 
 }
